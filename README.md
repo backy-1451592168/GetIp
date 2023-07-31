dd32700b97efacf59cca8f0a7390a239ca548907
sujunhao <1451592168@qq.com>
2023-07-31 18:00:52 +0800

    增加 IP物理地址查询

--- README.md
+++ README.md
@@ -5,3 +5,18 @@
 - 14.16.1
 ### 启动
 - node app.js
+
+### package.json文件自动生成
+- npm init -y
+
+
+## 容器生成
+```
+1、项目根目录下创建一个名为 "Dockerfile" 的文件
+
+2、构建 Docker 镜像，. 表示 Dockerfile 文件在当前目录
+  docker build -t your-name .
+
+3、运行容器，其中 "-p 7819:7819" 表示将容器内的 7819 端口映射到宿主机的 7819 端口，"-d" 表示在后台运行容器
+  docker run -p 7819:7819 -d your-name
+```
--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const ip2region = require('ip2region').default;
+const query = new ip2region();
 const app = express();
 
 app.get('/get-ip', (req, res) => {
-  // 从请求头中获取客户端的 IP 地址
-  const ipAddress = req.ip || req.connection.remoteAddress;
+  // 从请求头中获取客户端的 IP 地址，nginx代理转发
+  const xForwardedFor = req.headers['x-forwarded-for'];
+  const ipAddress = xForwardedFor ? xForwardedFor.split(',')[0] : req.connection.remoteAddress;
+  
   // 处理 IPv6 地址格式，移除 IPv6 地址的前缀 "::ffff:"
   const formattedIpAddress = ipAddress.replace('::ffff:', '');
 
+  const address = query.search(formattedIpAddress);
   let data = {
     ip: formattedIpAddress,
+    ...address,
     equipment: req.headers['sec-ch-ua-platform'],
     browser: req.headers['sec-ch-ua']
   }
   // 将获取到的 IP 地址发送给客户端
   // res.send(`Your IP address is: ${formattedIpAddress}`); // 返回body
-  res.json({ ...data })
+  res.json({  ...data })
 });
 
 const port = 7819;
